14a2a93af88df39918866cab93b52cad18f0431e
Jimin041115 <j243kim@uwaterloo.ca>
2025-06-08 18:34:28 -0400

    started popup page

---
+++ PlanEat/Pop-up_01.swift
@@ -0,0 +1,18 @@
+//
+//  Pop-up_01.swift
+//  PlanEat
+//
+//  Created by 김지민 on 6/8/25.
+//
+
+import SwiftUI
+
+struct Pop_up_01: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Pop_up_01()
+}
